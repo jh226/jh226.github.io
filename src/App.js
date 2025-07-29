9091b253a633e62aaaee37ca35656638bd0b8e45
이지현 <leejh020206@jiran.com>
2025-07-29 09:40:23 +0900

    🔥 app.css 삭제 및 app.js 수정

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './layout/Header';
+import DotNav from './layout/DotNav';
+import Footer from './layout/Footer';
+import Main from './pages/Main';
+import NotFound from './NotFound';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Test Page
-        </p>        
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	return (
+		<div className='App'>
+			<BrowserRouter>
+				<Header />
+				<DotNav />
+				<div style={{ paddingTop: '50px' }}></div>
+				<Routes>
+					<Route path="/portfolio" element={<Main />}></Route>
+
+					{/*일치하는 라우트가 없는경우 처리 */}
+					<Route path="*" element={<NotFound />}></Route>
+				</Routes>
+        		<Footer />
+			</BrowserRouter>
+		</div>
+	);
+};
 
 export default App;
